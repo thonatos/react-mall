d729dbcde5b0bdedced6ba9db01196ffcfd30f36
Thonatos.Yang <thonatos.yang@gmail.com>
2017-03-20 12:29:13 +0800

    componet: combine radio to radio container

---
+++ src/containers/order/components/RadioContainer.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Radio } from 'antd'
+
+const RadioButton = Radio.Button
+const RadioGroup = Radio.Group
+
+
+class RadioContainer extends Component {
+
+  state = {
+    value: 0
+  }
+
+  onChange = (e) => {
+    console.log('radio checked', e.target.value)
+
+    const { submitType, handleRadioChange, data } = this.props
+
+    this.setState({
+      value: e.target.value,
+    })
+    
+    if (handleRadioChange) {
+      handleRadioChange(submitType, data[this.state.value])
+    }
+  }
+
+  render() {
+
+    const { title, data } = this.props
+    let radioNodes = (<div></div>)
+
+    if (data) {
+      radioNodes = data.map((obj, key) =>
+        <RadioButton value={key} key={key}>{obj.name}</RadioButton>
+      )
+    }
+
+    return (
+      <div className="section">
+        <div className="header">
+          <h3>{title}</h3>
+        </div>
+        <RadioGroup onChange={this.onChange} value={this.state.value} defaultValue={this.state.value} className="radio-container">
+          {radioNodes}
+        </RadioGroup>
+      </div>
+    )
+  }
+}
+
+export default RadioContainer
