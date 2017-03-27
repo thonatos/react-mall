02d3efc9cd67d275c2042601c55f1fe9dfe4df0a
Thonatos.Yang <thonatos.yang@gmail.com>
2017-03-27 18:42:09 +0800

    update: order

--- src/actions/order.js
+++ src/actions/order.js
@@ -4,28 +4,40 @@ import { API_SERVER_SHOP } from '../config/'
 export const GET_ALL_META_SUCCESS = 'GET_ALL_META_SUCCESS'
 export const CREATE_ORDER_SUCCESS = 'CREATE_ORDER_SUCCESS'
 export const PAY_ORDER_SUCCESS = 'PAY_ORDER_SUCCESS'
+export const UPDATE_CART = 'UPDATE_CART'
 
 const GET_ALL_META = API_SERVER_SHOP + '/meta/getAll'
 const CREATE_ORDER = API_SERVER_SHOP + '/order/createOrder'
 const PAY_ORDER = API_SERVER_SHOP + '/payment/payOrder'
 
+function request(options, success, error) {
+  return axios({
+    url: options.url,
+    timeout: 20000,
+    method: options.method || 'get',
+    data: options.data || null,
+    headers: {
+      'X-Language': 'zh_cn',
+      'X-User-Token': '64651b5085bf35f9f6e12ee462de8dfb'
+    },
+    responseType: 'json'
+  }).then(function (response) {
+    success(response.data)
+  }).catch(function (response) {
+    console.log('#server', response)
+    error(response)
+  })
+}
 
 export function getAllMeta() {
   return (dispatch) => {
-    return axios({
-      url: GET_ALL_META,
-      timeout: 20000,
-      method: 'get',
-      headers: {
-        'X-Language': 'zh_cn'
-      },      
-      responseType: 'json'
-    }).then(function (response) {
-      const data = response.data
+    return request({
+      url: GET_ALL_META
+    }, (data) => {
       if (data.code === 0) {
         dispatch({ type: GET_ALL_META_SUCCESS, data: data.data })
       }
-    }).catch(function (response) {
+    }, (response) => {
       console.log('#server', response)
     })
   }
@@ -34,22 +46,15 @@ export function getAllMeta() {
 export function createOrder(order) {
   console.log(order)
   return (dispatch) => {
-    return axios({
+    return request({
       url: CREATE_ORDER,
-      timeout: 20000,
       method: 'post',
-      data: order,
-      headers: {
-        'X-User-Token': '64651b5085bf35f9f6e12ee462de8dfb'
-      },      
-      responseType: 'json'
-    }).then(function (response) {
-      const data = response.data
-      console.log(data)
+      data: order
+    }, (data) => {
       if (data.code === 0) {
         dispatch({ type: CREATE_ORDER_SUCCESS, data: data.data })
       }
-    }).catch(function (response) {
+    }, (response) => {
       console.log('#server', response)
     })
   }
@@ -58,23 +63,35 @@ export function createOrder(order) {
 export function payOrder(order) {
   console.log(order)
   return (dispatch) => {
-    return axios({
+    return request({
       url: PAY_ORDER,
-      timeout: 20000,
       method: 'post',
-      data: order,
-      headers: {
-        'X-User-Token': '64651b5085bf35f9f6e12ee462de8dfb'
-      },      
-      responseType: 'json'
-    }).then(function (response) {
-      const data = response.data
-      console.log(data)
+      data: order
+    }, (data) => {
       if (data.code === 0) {
         dispatch({ type: PAY_ORDER_SUCCESS, data: data.data })
       }
-    }).catch(function (response) {
+    }, (response) => {
       console.log('#server', response)
+    })
+  }
+}
+
+export function updateCart(data) {
+  const order = {
+    name: data.product.info.name,
+    key: data.product.info.name,
+    thumb: data.product.displays[0].url,
+    price: data.commodity.price,
+    count: 1,
+    _productId: data.product.product.id,
+    _commodityId: data.commodity.id
+  }
+
+  return (dispatch) => {
+    dispatch({
+      type: UPDATE_CART,
+      data: order
     })
   }
 }
--- src/containers/order/Info.js
+++ src/containers/order/Info.js
@@ -1,9 +1,7 @@
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import pingpp from 'pingpp-js'
 
 import * as orderActions from '../../actions/order'
-import * as shareActions from '../../actions/share'
 
 import React, { Component } from 'react'
 import { Row, Col, Button, message } from 'antd'
@@ -11,6 +9,7 @@ import { Row, Col, Button, message } from 'antd'
 import Cart from './components/Cart'
 import Contact from './components/Contact'
 import Delivery from './components/Delivery'
+import PayModal from './components/PayModal'
 import RadioContainer from './components/RadioContainer'
 
 import './Info.less'
@@ -19,59 +18,16 @@ import { user } from '../data'
 
 class Info extends Component {
 
-  state = {
-    submitStatus: 'disabled',
-    payStatus: 'disabled',
-    payEnabled: false,
-    delivery: 1
-  }
-
   componentDidMount() {
-    const { share, getAllMeta } = this.props
+    const { getAllMeta, cart } = this.props
     getAllMeta()
 
-    // init
-    if (!share.cart) {
+    if (cart.length < 1) {
       message.warn('你当前购物车为空.')
-    } else {
-      this.setState({
-        items: [
-          {
-            id: share.cart.commodity.id,
-            number: 1
-          }
-        ],
-        submitStatus: false
-      })
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    const { order } = nextProps
-    console.log(order)
-
-    if (order.order && Object.keys(order.order).length > 0) {
-      this.setState({
-        payStatus: false
-      })
-    }
-
-    if (order.payData && Object.keys(order.payData).length > 0) {
-      console.log(order.payData)
-      pingpp.createPayment(order.payData, function (result, err) {
-        console.log(result, err)
-        if (result === "success") {
-          // 只有微信公众账号 wx_pub 支付成功的结果会在这里返回，其他的支付结果都会跳转到 extra 中对应的 URL。
-        } else if (result === "fail") {
-          // charge 不正确或者微信公众账号支付失败时会在此处返回
-        } else if (result === "cancel") {
-          // 微信公众账号支付取消支付
-        }
-      })
-    }
-  }
-
-  handleChildSubmit = (type, values) => {    
+  handleChildSubmit = (type, values) => {
     console.log(type, values)
     let state = {}
     switch (type) {
@@ -81,7 +37,11 @@ class Info extends Component {
           title: values.title || 'none'
         }
         break;
-      case 'contact':        
+      case 'contact':
+        state[type] = values
+        break;
+      case 'delivery':
+        console.log(values)
         state[type] = values
         break;
       default:
@@ -89,7 +49,7 @@ class Info extends Component {
         break;
     }
 
-    this.setState(state, ()=>{
+    this.setState(state, () => {
       console.log(this.state)
     })
   }
@@ -97,10 +57,6 @@ class Info extends Component {
 
   handleSubmit = (e) => {
     const { createOrder } = this.props
-    if (this.state.items.length < 1) {
-      console.log('items')
-      return false
-    }
     if (!this.state.delivery) {
       console.log('delivery')
       return false
@@ -125,29 +81,14 @@ class Info extends Component {
   }
 
   render() {
-    const { share, order } = this.props
-
-    let cart = (<div></div>)
-
-    if (share.cart) {
-      let _cart = [{
-        count: 1,
-        name: share.cart.product.info.name,
-        key: share.cart.product.info.name,
-        thumb: share.cart.product.displays[0].url,
-        price: share.cart.commodity.price
-      }]
-      cart = (<Cart data={_cart}></Cart>)
-    }
-
+    const { order, cart, showPayModal, submitStatus } = this.props
     return (
       <Row className="container order">
 
+        <PayModal visible={showPayModal}></PayModal>
+
         <Col span={24}>
-          <Delivery data={{
-            order: order,
-            user: user
-          }} handleResult={this.handleDistribution}></Delivery>
+          <Delivery submitType='delivery' data={{ order, user }} handleRadioChange={this.handleChildSubmit}></Delivery>
         </Col>
 
         <Col span={14}>
@@ -168,12 +109,11 @@ class Info extends Component {
         </Col>
 
         <Col span={24}>
-          {cart}
+          <Cart data={cart}></Cart>
         </Col>
 
         <Col span={24} className="action-block">
-          <Button type="primary" disabled={this.state.payStatus} onClick={this.handlePay}>支付订单</Button>
-          <Button type="primary" disabled={this.state.submitStatus} onClick={this.handleSubmit}>提交订单</Button>
+          <Button type="primary" disabled={submitStatus} onClick={this.handleSubmit}>提交订单</Button>
           <p className="tips">点击提交订单表示您同意 <a href="#">Insta360 商城的销售政策</a></p>
         </Col>
 
@@ -184,13 +124,15 @@ class Info extends Component {
 
 function mapStateToProps(state) {
   return {
-    share: state.share,
-    order: state.order
+    order: state.order,
+    cart: state.order.cart,
+    showPayModal: false,
+    submitStatus: state.order.submitStatus
   }
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators(Object.assign({}, orderActions, shareActions), dispatch)
+  return bindActionCreators(orderActions, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Info)
