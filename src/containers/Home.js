4cceb0410caf1f94898895ffc95ede9ae8d18283
Thonatos.Yang <thonatos.yang@gmail.com>
2017-03-30 19:01:23 +0800

    add shortcut

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 
 class Home extends Component {
-  
+
   render() {
+
+    const links = [
+      ['/user/login', '登录'],
+      ['/user/register', '注册'],
+      ['/product/pro', '产品页'],
+      ['/order/list', '订单列表']
+    ]
+
     return (
       <div className="container">
         <div style={{
           padding: '8em 0'
-        }}>          
-          <p>Coming Soon..</p>
+        }}>
+          <p>快捷入口</p>
+          <div style={{
+            textAlign: 'right'
+          }}>
+            {
+              links.map((v, k) => {
+                return (<Link to={v[0]} key={k} style={{
+                  margin: '0 1em'
+                }}>{v[1]}</Link>)
+              })
+            }
+          </div>
         </div>
       </div>
     )
