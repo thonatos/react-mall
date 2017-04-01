c8b90c6eeecddcd9ff3fa963661e27f9bee9b404
Thonatos.Yang <thonatos.yang@gmail.com>
2017-04-01 18:19:55 +0800

    app: add conf for dev & prod

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./production')
-} else {
-  module.exports = require('./development')
-}
+module.exports = require('./production')
+
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./production')
+// } else {
+//   module.exports = require('./development')
+// }
