6b7664d450d35cb32f05d7b28ef3734f1284d54d
Thonatos.Yang <thonatos.yang@gmail.com>
2017-04-10 19:36:29 +0800

    update order list page

--- src/containers/order/List.js
+++ src/containers/order/List.js
@@ -5,44 +5,101 @@ import * as orderActions from '../../actions/order'
 import * as authActions from '../../actions/auth'
 
 import React, { Component } from 'react'
-import { Row, Col, Tabs, Menu } from 'antd'
+import { Row, Col, Table, Button } from 'antd'
 
-const TabPane = Tabs.TabPane
-
-import Orders from './components/Orders'
 import './List.less'
 
-// import { user } from '../data'
-
 class List extends Component {
-
-  state = {
-    current: '1',
-  }
 
   componentDidMount() {
     const { getUserOrders, restoreAuth } = this.props.actions
     restoreAuth()
-    getUserOrders('0')
+    getUserOrders()
   }
 
-  handleClick = (e) => {
-    this.setState({
-      current: e.key,
-    });
-  }
-
-  callback = (key) => {
-    const { getUserOrders } = this.props.actions
-    getUserOrders(key)
+  onClick = (action, value) => {
+    console.log(action, value)
   }
 
   render() {
-
     const { orders, auth } = this.props.reducer
+
     const profile = {
       avatar: 'https://static.insta360.cn/assets/mall/ic_avatar@2x.png',
       mail: auth.email
+    }
+
+    const columns = [
+      {
+        title: 'Thumb',
+        dataIndex: 'thumb',
+        key: 'thumb',
+        render: (text, record, index) => {
+          return (
+            <img style={{
+              maxWidth: '50px'
+            }} src={record.thumb} alt="" />
+          )
+        }
+      },
+      {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email'
+      },
+      {
+        title: 'Order Number',
+        dataIndex: 'on',
+        key: 'on'
+      },
+      {
+        title: 'Created Time',
+        dataIndex: 'created',
+        key: 'created'
+      },
+      {
+        title: 'Payment',
+        dataIndex: 'payment',
+        key: 'payment'
+      },
+      {
+        title: 'State',
+        dataIndex: 'state',
+        key: 'state'
+      },      
+      {
+        title: 'Action',
+        dataIndex: 'action',
+        key: 'action',
+        render: (text, record, index) => {          
+          if (record.state === 0) {
+            return (<div>
+              <Button className="action-btn" onClick={this.onClick.bind(this, 'view', record)}>View</Button>
+              <Button className="action-btn" onClick={this.onClick.bind(this, 'pay', record)}>Pay Now</Button>
+              <Button className="action-btn" onClick={this.onClick.bind(this, 'cancel', record)}>Cancel</Button>
+            </div>)
+          }
+          return (<Button className="action-btn" onClick={this.onClick.bind(this, 'view', record)}>View</Button>)
+        }
+      }
+    ]
+
+    let dataSource = []
+    if (Array.isArray(orders) && orders.length > 0) {
+      dataSource = orders.map((item, key) => {
+        return {
+          thumb: 'https://www.insta360.com/public/images/v6/download/air@1x.png',
+          key: item.id,
+          id: item.id,
+          account: item.account,
+          email: item.contact_email || 'none',
+          on: item.order_number,
+          created: new Date(item.create_time).toISOString(),
+          state: item.state,
+          payment: item.pay_type,
+          action: item.state
+        }
+      })
     }
 
     return (
@@ -54,58 +111,18 @@ class List extends Component {
           </div>
         </div>
 
-        <Row className="container content">
-          <Col span={8} className="sidebar">
-            <Menu
-              theme={'light'}
-              onClick={this.handleClick}
-              style={{ width: 240 }}
-              selectedKeys={[this.state.current]}
-              mode="inline"
-            >
-              <Menu.Item key="1">Account</Menu.Item>
-              <Menu.Item key="2">Orders</Menu.Item>
-            </Menu>
+        <Row className="container detail">
 
+          <Col span={6} className="account">
+            <img src={profile.avatar} alt="" style={{ maxWidth: '100px' }} />
+            <p>{profile.mail}</p>
           </Col>
 
-          <Col span={16} className="info">
-            {/* Account */}
-            <Col span={24} className="account">
-              <Row type="flex" align="middle">
-                <Col span={4}>
-                  <img src={profile.avatar} alt="" style={{ maxWidth: '100px' }} />
-                </Col>
-                <Col span={20}>
-                  <h2>{profile.mail}</h2>
-                  <p>{profile.mail}</p>
-                </Col>
-              </Row>
-            </Col>
-
-            {/* Detail */}
-            <Col span={24} className="orders">
-              <Tabs defaultActiveKey={this.state.current} onChange={this.callback}>
-                <TabPane tab="Init" key="0">
-                  <Orders data={orders.init}></Orders>
-                </TabPane>
-                <TabPane tab="Payed" key="1">
-                  <Orders data={orders.payed}></Orders>
-                </TabPane>
-                <TabPane tab="Prepared" key="2">
-                  <Orders data={orders.prepared}></Orders>
-                </TabPane>
-                <TabPane tab="On Delivery" key="3">
-                  <Orders data={orders.onDelivery}></Orders>
-                </TabPane>
-                <TabPane tab="Success" key="9">
-                  <Orders data={orders.success}></Orders>
-                </TabPane>
-                <TabPane tab="Cancel" key="-1">
-                  <Orders data={orders.canceled}></Orders>
-                </TabPane>
-              </Tabs>
-            </Col>
+          <Col span={18} className="info">
+            <Table pagination={false}
+              columns={columns}
+              dataSource={dataSource}              
+            />
           </Col>
         </Row >
       </div>
